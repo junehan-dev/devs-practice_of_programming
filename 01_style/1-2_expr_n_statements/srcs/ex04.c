4ad7394e0340926001f4de61adca852e9f047d7a
junehan <junehan.dev@gmail.com>
2022-01-01 16:50:29 +0900

    EX/1-2/ex04/FIN

--- 01_style/1-2_expr_n_statements/srcs/ex04.c
+++ 01_style/1-2_expr_n_statements/srcs/ex04.c
@@ -7,9 +7,27 @@ int	old(void)
 	flag = flag ? 0 : 1;
 	quote = (*line == '"') ? 1 : 0;
 
-	if (val % 1)
+	if (val & 1)
 		bit = 1;
 	else
 		bit = 0;
+}
+
+int	improved(void)
+{
+	if (c != 'y' && c != 'Y')
+		return;
+
+	if (length < BUFSIZE) // length is already set to length.
+		length = BUFSIZE;
+
+	flag != flag; // simple reverting does not need ternary operator.
+
+	if (*line == '"') // not need to set quote to store value. except when to remove quote now and do something later depend on quoted or not.
+		print_line(line);
+	else
+		print_line((remove_quote(line));
+
+	bit = (val & 1); // val & 1 is 1 or 0. no if-statement useful at this time.
 }
 
