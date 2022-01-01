abdd25dbbc6453011a1b096bd7837ca10240c5a9
junehan <junehan.dev@gmail.com>
2022-01-01 17:04:04 +0900

    EX/1-2/ex06/FIN

--- 01_style/1-2_expr_n_statements/srcs/ex06.c
+++ 01_style/1-2_expr_n_statements/srcs/ex06.c
@@ -1,5 +1,15 @@
-int	test(void)
+#include <stdio.h>
+
+int	main(void)
 {
 	int n = 1;
 	printf("%d %d\n", n++, n++);
+	/*
+	 * print 1, 1 and n is 3.
+	 * print 1, 2 and n is 3. (printf동작상 이게 맞지 않을까.?)
+	 * print 2, 3 and n is 3.
+	 *
+	 * Answer
+	 * print 2, 1 and n is 3.
+	 */
 }
