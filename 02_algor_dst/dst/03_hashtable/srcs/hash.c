1a649832dbdbe5400a9493b9463867e70795c8ec
junehan <junehan.dev@gmail.com>
2022-03-16 17:24:02 +0900

    2/DST/hashtable/common hash algorithm

---
+++ 02_algor_dst/dst/03_hashtable/srcs/hash.c
@@ -0,0 +1,18 @@
+#define MULTIPLIER	37
+/* 31, 37 for ascii */
+#define NHASH		1028
+/* arraySize for Hash values */
+
+unsigned int	hash(char *str)
+{
+	unsigned int	h;
+	unsigned char	*p;
+
+	h = 0;
+	p = (unsigned char *)str;
+	while (*p) {
+		h = MULTIPLIER * h + *p;
+		p++;
+	}
+	return  h % NHASH
+}
